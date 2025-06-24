44de8b8c1e2edc65c197b257221876c75c0cf258
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-06-24 16:43:35 +0200

    logout

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,4 +1,14 @@
 import { defineStore } from 'pinia';
+import { useOrderStore } from '@/stores/order';
+import { useServiceStore } from '@/stores/service';
+import { useScheduleStore } from '@/stores/schedule';
+import { useTransportStore } from '@/stores/transport';
+import { useCustomerRuleStore } from '@/stores/customerRule';
+import { useCustomerGroupStore } from '@/stores/customerGroup';
+import { useDeliveryGroupStore } from '@/stores/deliveryGroup';
+import { useGeographicZoneStore } from '@/stores/geographicZone';
+import { useCollectionPointStore } from '@/stores/collectionPoint';
+import { useAdministrationUserStore } from '@/stores/administrationUser';
 
 export const useUserStore = defineStore('user', {
   state: () => ({
@@ -6,5 +16,19 @@ export const useUserStore = defineStore('user', {
     userId: 0
   }),
   actions: {
+    logout(router) {
+      this.$reset();
+      useOrderStore().$reset();
+      useServiceStore().$reset();
+      useScheduleStore().$reset();
+      useTransportStore().$reset();
+      useCustomerRuleStore().$reset();
+      useCustomerGroupStore().$reset();
+      useDeliveryGroupStore().$reset();
+      useGeographicZoneStore().$reset();
+      useCollectionPointStore().$reset();
+      useAdministrationUserStore().$reset();
+      router.push('/');
+    }
   }
 });
