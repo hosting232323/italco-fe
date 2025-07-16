6d4ed5f2a9f95c61bf0a65d8edeb57f669877197
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-07-16 15:02:48 +0200

    push

--- src/utils/http.js
+++ src/utils/http.js
@@ -4,10 +4,10 @@ import { storeToRefs } from 'pinia';
 
 const hostname = import.meta.env.VITE_HOSTNAME;
 
-const postRequest = (endpoint, body, func, method = 'POST', router = undefined, file = false) => {
+const postRequest = async (endpoint, body, func, method = 'POST', router = undefined, file = false) => {
   fetch(`${hostname}${endpoint}`, {
     method: method,
-    headers: createHeader(router),
+    headers: await createHeader(router),
     body: JSON.stringify(body)
   }).then(response => {
     if (!response.ok)
@@ -20,13 +20,13 @@ const postRequest = (endpoint, body, func, method = 'POST', router = undefined, 
   });
 };
 
-const formDataRequest = (endpoint, data, func, method = 'POST', router = undefined) => {
+const formDataRequest = async (endpoint, data, func, method = 'POST', router = undefined) => {
   const formData = new FormData();
   Object.keys(data).forEach(key => formData.append(key, data[key]));
 
   fetch(`${hostname}${endpoint}`, {
     method: method,
-    headers: createHeader(router, true),
+    headers: await createHeader(router, true),
     body: formData
   }).then(response => {
     if (!response.ok)
@@ -40,13 +40,13 @@ const formDataRequest = (endpoint, data, func, method = 'POST', router = undefin
 };
 
 
-const getRequest = (endpoint, params, func, method = 'GET', router = undefined, file = false) => {
+const getRequest = async (endpoint, params, func, method = 'GET', router = undefined, file = false) => {
   const url = new URL(`${hostname}${endpoint}`);
   Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 
   fetch(url, {
     method: method,
-    headers: createHeader(router, file)
+    headers: await createHeader(router, file)
   }).then(response => {
     if (!response.ok)
       throw new Error(`Errore nella risposta del server: ${response.status} - ${response.statusText}`);
@@ -69,24 +69,23 @@ const createHeader = async (router, file = false) => {
   if (router)
     headers['Authorization'] = localStorage.getItem('token');
 
-  // const userStore = useUserStore();
-  // const { role } = storeToRefs(userStore);
+  headers['X-Lat'] = '';
+  headers['X-Lon'] = '';
 
-  // if (role.value === 'Delivery' && navigator.geolocation) {
-  //   try {
-  //     const position = await new Promise((resolve, reject) => {
-  //       navigator.geolocation.getCurrentPosition(resolve, reject, { timeout: 3000 });
-  //     });
+  const userStore = useUserStore();
+  const { role } = storeToRefs(userStore);
 
-  //     headers['lat'] = position.coords.latitude;
-  //     headers['lon'] = position.coords.longitude;
-
-  //   } catch (error) {
-  //     console.warn('Posizione non disponibile:', error.message);
-  //   }
-  // }
-
-  console.log(headers);
+  if (role.value === 'Delivery' && navigator.geolocation) {
+    try {
+      const position = await new Promise((resolve, reject) =>
+        navigator.geolocation.getCurrentPosition(resolve, reject, { timeout: 3000 })
+      );
+      headers['X-Lat'] = position.coords.latitude;
+      headers['X-Lon'] = position.coords.longitude;
+    } catch (error) {
+      console.warn('Posizione non disponibile:', error.message);
+    }
+  }
 
   return headers;
 };
