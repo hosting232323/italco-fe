841247c714455897430879c64d4685cec3c1b0c4
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-10-06 16:18:56 +0200

    update-delivery-group

--- src/views/administration/AdministrationUsers.vue
+++ src/views/administration/AdministrationUsers.vue
@@ -16,55 +16,25 @@
         </h1><hr>
         <UserForm />
         <UserTable />
-        <h1>
-          Gruppi Delivery
-          <v-btn
-            icon="mdi-plus"
-            style="float: right;"
-            variant="text"
-            @click="openDeliveryGroupForm"
-          />
-        </h1><hr class="mt-2">
-        <DeliveryGroupForm />
-        <DeliveryGroupTable @open-pop-up="openPopUp" />
       </v-container>
-    </template>
-    <template #default>
-      <DeliveryGroupPopUp />
     </template>
   </v-dialog>
 </template>
 
 <script setup>
-import UserForm from '@/components/administration/users/UserForm';
-import UserTable from '@/components/administration/users/UserTable';
-import DeliveryGroupForm from '@/components/administration/users/DeliveryGroupForm';
-import DeliveryGroupPopUp from '@/components/administration/users/DeliveryGroupPopUp';
-import DeliveryGroupTable from '@/components/administration/users/DeliveryGroupTable';
+import UserForm from '@/components/administration/users/Form';
+import UserTable from '@/components/administration/users/Table';
 
 import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
-import { useDeliveryGroupStore } from '@/stores/deliveryGroup';
 import { useAdministrationUserStore } from '@/stores/administrationUser';
 
 const popUp = ref(false);
-const deliveryGroupStore = useDeliveryGroupStore();
 const administrationUserStore = useAdministrationUserStore();
 const { activeForm: userForm, element: user } = storeToRefs(administrationUserStore);
-const { activeForm: deliveryGroupForm, element: deliveryGroup } = storeToRefs(deliveryGroupStore);
-
-const openPopUp = (item) => {
-  popUp.value = true;
-  deliveryGroup.value = item;
-};
 
 const openUserForm = () => {
   user.value = {};
   userForm.value = true;
-};
-
-const openDeliveryGroupForm = () => {
-  deliveryGroup.value = {};
-  deliveryGroupForm.value = true;
 };
 </script>
