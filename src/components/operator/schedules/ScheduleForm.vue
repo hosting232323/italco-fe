c9a8189b142bd2d1f478d8efa2a4b83fec63e94d
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2025-10-19 03:56:34 +0200

    push

--- src/components/operator/schedules/ScheduleForm.vue
+++ src/components/operator/schedules/ScheduleForm.vue
@@ -181,6 +181,9 @@ const availableUsers = computed(() => {
 
 const removeUser = (userId) => {
   schedule.value.users = schedule.value.users.filter(u => u.id !== userId);
+    if (!schedule.value.deleted_users)
+    schedule.value.deleted_users = [];
+  schedule.value.deleted_users.push(userId);
 };
 
 const addOrder = () => {
