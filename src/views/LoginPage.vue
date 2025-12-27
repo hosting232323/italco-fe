28922677335d6a1169518a9f5bbc6e8b500f81d2
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-12-27 13:18:45 +0100

    login and module refactor

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -9,15 +9,30 @@
     :hostname="hostname"
     :iv="iv"
     :secret-key="secretKey"
+    @call-back="goToDashboard"
   />
 </template>
 
 <script setup>
 import { useTheme } from 'vuetify';
+import { storeToRefs } from 'pinia';
+import { useRouter } from 'vue-router';
 import { AuthManager } from 'generic-module';
+import { useUserStore } from '@/stores/user';
 
-const theme = useTheme();
 const iv = import.meta.env.VITE_IV;
 const hostname = import.meta.env.VITE_HOSTNAME;
 const secretKey = import.meta.env.VITE_SECRET_KEY;
+
+const theme = useTheme();
+const router = useRouter();
+const userStore = useUserStore();
+const { role, userId, token } = storeToRefs(userStore);
+
+const goToDashboard = (data) => {
+  role.value = data.role;
+  userId.value = data.user_id;
+  token.value = data.token;
+  router.push('dashboard');
+};
 </script>
