b1fc44f512451e2e77708cfa35374b53ffcd41d3
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-01-30 16:26:28 +0100

    push

--- src/views/administration/AdministrationLog.vue
+++ src/views/administration/AdministrationLog.vue
@@ -3,12 +3,12 @@
     <h1>
       Logs
     </h1><hr>
-    <LogTable />
     <LogFilters />
+    <LogTable />
   </v-container>
 </template>
 
 <script setup>
-import LogTable from '@/components/administration/users/LogTable';
 import LogFilters from '@/components/administration/users/LogFilters';
+import LogTable from '@/components/administration/users/LogTable';
 </script>
