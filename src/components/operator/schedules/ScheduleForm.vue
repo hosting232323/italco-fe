8b40730ea52ef95343d3a4403dabf57921a0dbbf
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-02-22 06:54:26 +0000

    New map

--- src/components/operator/schedules/ScheduleForm.vue
+++ src/components/operator/schedules/ScheduleForm.vue
@@ -154,7 +154,7 @@
           md="5"
         >
           <div style="height: 100%; border-radius: 12px; overflow: hidden;">
-            <!-- <GoogleMap v-if="schedule.schedule_items && schedule.schedule_items.length > 0" /> -->
+            <OverStreetMap v-if="schedule.schedule_items && schedule.schedule_items.length > 0" />
           </div>
         </v-col>
       </v-row>
@@ -163,8 +163,8 @@
 </template>
 
 <script setup>
-// import GoogleMap from '@/components/GoogleMap.vue';
 import FormButtons from '@/components/FormButtons';
+import OverStreetMap from '@/components/OverStreetMap.vue';
 
 import { ref, watch } from 'vue';
 import { useTheme } from 'vuetify';
