2dd8f5037d0b9c0e37dae19b6210ecf852e1f852
cl_cassanelli <cldevofficial@gmail.com>
2026-03-06 17:23:01 +0100

    push

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (_req, res) => {
+app.get('/*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
