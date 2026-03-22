042c712a2c89f3211ac3c09420542ab793703bdf
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-03-22 00:41:20 +0000

    Logs preview

--- src/components/administration/logs/LogTable.vue
+++ src/components/administration/logs/LogTable.vue
@@ -7,9 +7,18 @@
       { title: 'ID', value: 'logs.id', sortable: false },
       { title: 'Data', value: 'logs.created_at', sortable: false },
       { title: 'User', value: 'user.nickname', sortable: false },
-      { title: 'Log', value: 'logs.content', sortable: false }
+      { title: 'Azioni', key: 'actions', sortable: false }
     ]"
-  />
+  >
+    <template #[`item.actions`]="{ item }">
+      <v-btn
+        icon="mdi-magnify-plus-outline"
+        variant="text"
+        :color="theme.current.value.primaryColor"
+        @click="openForm(item.logs)"
+      />
+    </template>
+  </v-data-table>
 </template>
 
 <script setup>
@@ -22,8 +31,13 @@ import { useLogStore } from '@/stores/log';
 const theme = useTheme();
 const router = useRouter();
 const logStore = useLogStore();
-const { ready } = storeToRefs(logStore);
+const { ready, activePopUp, selectedLog } = storeToRefs(logStore);
 const logs = storesUtils.getStoreList(logStore, router);
+
+const openForm = (item) => {
+  selectedLog.value = item.id;
+  activePopUp.value = true;
+};
 </script>
 
 <style scoped>
