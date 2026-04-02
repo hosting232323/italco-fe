65733757f6a835994e793a4f1288a84033455aea
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-04-02 16:17:10 +0200

    history

--- src/components/orders/StatusPopup.vue
+++ src/components/orders/StatusPopup.vue
@@ -3,16 +3,22 @@
     <v-data-table
       :items="statuses"
       :headers="[
-        { title: 'Stato', value: 'status', sortable: false },
+        { title: 'Tipo/Status', value: 'status', sortable: false },
         { title: 'Data', value: 'date', sortable: false },
         { title: 'Ora', value: 'time', sortable: false }
       ]"
     >
       <template #[`item.status`]="{ item }">
-        <v-chip :color="orderUtils.LABELS.find(label => label.value == item.status).color">
-          {{ orderUtils.LABELS.find(label => label.value == item.status).title }}
-        </v-chip>
+        <template v-if="item.status">
+          <v-chip :color="orderUtils.LABELS.find(label => label.value == item.status).color">
+            {{ orderUtils.LABELS.find(label => label.value == item.status).title }}
+          </v-chip>
+        </template>
+        <template v-else>
+            {{ formatOtherStatus(item) }}
+        </template>
       </template>
+
       <template #[`item.date`]="{ item }">
         {{ item.created_at.split(' ')[0] }}
       </template>
@@ -32,4 +38,15 @@ const { statuses } = defineProps({
     required: true
   }
 });
+
+const formatOtherStatus = (item) => {
+  const keys = Object.keys(item).filter(
+    k => !['id', 'order_id', 'created_at', 'updated_at', 'status'].includes(k)
+  );
+  
+  if (keys.length) {
+    return `${keys[0]}: ${item[keys[0]]}`;
+  }
+  return '';
+};
 </script>
