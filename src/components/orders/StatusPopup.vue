bf80680f31e25cc7ce324fdfcdad1a12781a1811
Vanni <hosting23232323@gmail.com>
2026-04-04 12:31:43 +0000

    History

--- src/components/orders/StatusPopup.vue
+++ src/components/orders/StatusPopup.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card title="Storico Stati">
+  <v-card title="Storico Ordine">
     <v-data-table
       :items="statuses"
       :headers="[
@@ -9,9 +9,28 @@
       ]"
     >
       <template #[`item.status`]="{ item }">
-        <v-chip :color="orderUtils.LABELS.find(label => label.value == item.status).color">
+        <v-chip
+          v-if="item.status"
+          :color="orderUtils.LABELS.find(label => label.value == item.status).color"
+        >
           {{ orderUtils.LABELS.find(label => label.value == item.status).title }}
         </v-chip>
+        <div v-else-if="item.hasOwnProperty('anomaly')">
+          <v-icon :color="item.anomaly ? 'red' : 'green'" class="me-1">
+            {{ item.anomaly ? 'mdi-alert-outline' : 'mdi-alert-remove-outline' }}
+          </v-icon>
+          Anomalia
+        </div>
+        <div v-else-if="item.hasOwnProperty('delay')">
+          <v-icon :color="item.delay ? 'orange' : 'blue'" class="me-1">
+            {{ item.delay ? 'mdi-clock-alert-outline' : 'mdi-clock-outline' }}
+          </v-icon>
+          Ritardo
+        </div>
+        <div v-else-if="item.hasOwnProperty('confirmed')" class="d-flex align-center">
+          <div :style="{width: '15px', height: '15px', borderRadius: '50%', marginRight: '8px' ,backgroundColor: item.confirmed ? 'green' : 'red'}"></div>
+          Confermato
+        </div>
       </template>
       <template #[`item.date`]="{ item }">
         {{ item.created_at.split(' ')[0] }}
