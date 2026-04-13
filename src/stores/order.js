7d6e2753833412c7e6ebc204269ac496447220de
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-04-13 09:00:44 +0200

    new date filters

--- src/stores/order.js
+++ src/stores/order.js
@@ -52,13 +52,10 @@ export const useOrderStore = defineStore('order', {
       );
     },
     initList(router) {
-      Object.keys(this.filters).forEach(key => {
-        if (!this.filters[key]) delete this.filters[key];
-      });
-
       http.postRequest(
         'order/filter',
-        {filters: storesUtils.formatFilters({ ...this.filters }, 'Order.dpc')},
+        {filters: storesUtils.formatFilters({ ...this.filters },
+          storesUtils.ORDER_DATE_FILTER_TYPES, 'Order')},
         this.setList,
         'POST',
         router
