ae2d8953a30ef7d0cc0bb3e09bc50d1d21f75017
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-04-20 17:21:56 +0200

    fix di vanni

--- src/components/administration/raeProducts/RaeProductTable.vue
+++ src/components/administration/raeProducts/RaeProductTable.vue
@@ -78,20 +78,14 @@ const raeProducts = storesUtils.getStoreList(raeProductStore, router);
 const raeExport = (item) => {
   exportLoading[item.id] = true;
 
-  http.getRequest(`export/rae/${item.order.id}`, {}, function (data) {
-    exportLoading[item.id] = false;
-    if (data.status == 'ko')
-      alert(data.error);
-    else {
-      const blob = new Blob([data], { type: 'application/pdf' });
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement('a');
-      a.href = url;
-      a.download = `raee_${item.order.id}.pdf`;
-      a.click();
-      window.URL.revokeObjectURL(url);
-    }
-  }, 'GET', router);
+  http.downloadRequest(
+    `export/rae/${item.order.id}`, 
+    {},
+    'GET',
+    router,
+    `raee_${item.order.id}.pdf`,
+    () => exportLoading[item.id] = false
+  );
 };
 
 const deleteItem = (item) => {
