9cc9a7116fa2a7b630cf95102a9c7439890b0539
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-05-07 21:21:53 +0200

    new-chatty

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -3,7 +3,10 @@
     <NavigationDrawer />
     <v-main :style="{ backgroundColor: theme.current.value.secondaryColor }">
       <router-view />
-      <ChattyBot />
+      <ChattyBot 
+        :hostname="viteHostname"
+        :bot-data="botLanding"
+      />
     </v-main>
     <Footer />
   </v-app>
@@ -11,7 +14,7 @@
 
 <script setup>
 import Footer from '@/layouts/FooterBar';
-import ChattyBot from '@/layouts/ChattyBot';
+import { ChattyBot } from 'generic-module';
 import NavigationDrawer from '@/layouts/NavigationDrawer';
 
 import { onMounted } from 'vue';
@@ -19,6 +22,21 @@ import { useTheme } from 'vuetify';
 import { loadGoogleMapsScript } from '@/utils/googleMapsScript';
 
 const theme = useTheme();
+const viteHostname = import.meta.env.VITE_HOSTNAME;
 
 onMounted(() => loadGoogleMapsScript());
+
+const botLanding = {
+  id: 3,
+  message: 'Ciao! Sono qui per rispondere alle tue domande sugli ordini.<br>Chiedimi quello che ti serve sapere specificando la data di creazione degli ordini interessati.',
+  name: 'Italco.mi Bot',
+  image: '/logo.png',
+  color: {
+    theme_color: 'rgb(53, 76, 124)',
+    theme_color_hover: 'rgb(71, 96, 156)',
+    fab_hover: 'rgb(71, 96, 156)',
+    fab_shadow: 'rgba(38, 55, 90, 0.5)',
+    fab_border: 'rgb(38, 55, 90)'
+  },
+};
 </script>
