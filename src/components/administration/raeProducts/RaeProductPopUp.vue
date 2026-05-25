b39fdc7cb55b67917307812a0ad45f46912199ae
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-05-25 11:43:22 +0200

    push

--- src/components/administration/raeProducts/RaeProductPopUp.vue
+++ src/components/administration/raeProducts/RaeProductPopUp.vue
@@ -16,6 +16,51 @@
           )"
           :rules="validation.requiredRules"
         />
+          <v-file-input
+              label="File PDF"
+              accept="application/pdf"
+              :rules="validation.requiredRules"
+              :error-messages="fileError"
+              @change="onFilesSelected"
+            />
+            <div
+              v-if="file"
+              class="mb-4"
+            >
+              <strong>PDF selezionato</strong>
+
+              <v-card class="pdf-card">
+                <v-card-text class="text-center">
+                  <v-icon
+                    size="64"
+                    color="red"
+                  >
+                    mdi-file-pdf-box
+                  </v-icon>
+
+                  <div class="pdf-name mt-2">
+                    {{ file.selectedFile.name }}
+                  </div>
+                </v-card-text>
+
+                <v-divider />
+
+                <v-card-actions class="justify-center">
+                  <v-btn
+                    icon="mdi-eye"
+                    variant="text"
+                    @click="openPdf(file)"
+                  />
+
+                  <v-btn
+                    icon="mdi-delete"
+                    variant="text"
+                    color="red"
+                    @click="file = null"
+                  />
+                </v-card-actions>
+              </v-card>
+            </div>
         <FormButtons
           :loading="loading"
           @cancel="activePopUpForm = false"
@@ -38,7 +83,10 @@ import storesUtils from '@/utils/stores';
 import validation from '@/utils/validation';
 import { useRaeProductStore } from '@/stores/raeProduct';
 
+const file = ref(null);
 const form = ref(null);
+const fileError = ref('');
+
 const raeProductStore = useRaeProductStore();
 const { element: rae, activePopUpForm } = storeToRefs(raeProductStore);
 
@@ -46,5 +94,22 @@ const submitForm = async () => {
   if (!(await form.value.validate()).valid) return;
 
   loading.value = true;
+};
+
+const onFilesSelected = (event) => {
+  const selectedFile = event.target.files?.[0];
+  if (!selectedFile) return;
+
+  fileError.value = '';
+
+  if (selectedFile.type !== 'application/pdf') {
+    fileError.value = 'Puoi caricare solo file PDF';
+    return;
+  }
+
+  file.value = {
+    selectedFile,
+    preview: URL.createObjectURL(selectedFile),
+  };
 };
 </script>
