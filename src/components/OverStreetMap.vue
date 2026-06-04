dfaa891ddc1783c1723e37ea0b5f5b2fd0fed1b9
Giovanni Colasanto <X1068076@guest.telecomitalia.it>
2026-06-04 11:18:36 +0200

    nominatim

--- src/components/OverStreetMap.vue
+++ src/components/OverStreetMap.vue
@@ -75,10 +75,9 @@ const { element: schedule } = storeToRefs(scheduleStore);
 const emits = defineEmits(['not-found-addresses']);
 
 const geocode = async (address) => {
-  const res = await fetch(
-    `https://wooffy.it/nominatim/search?format=json&q=${encodeURIComponent(address)}`
-  );
+  const res = await fetch(`/nominatim/search?format=json&q=${encodeURIComponent(address)}`);
   const data = await res.json();
+
   return data[0]
     ? { lat: +data[0].lat, lng: +data[0].lon }
     : null;
