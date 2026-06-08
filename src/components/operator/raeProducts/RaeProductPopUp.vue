f4d5916a23d7d63760e5c11aa9faa97ba25aea4a
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-06-08 18:03:13 +0200

    Revert "smaltimento"
    This reverts commit b6d75a715df59214b44ee7fccd406a943462a89f.

--- src/components/operator/raeProducts/RaeProductPopUp.vue
+++ src/components/operator/raeProducts/RaeProductPopUp.vue
@@ -4,28 +4,123 @@
     class="mt-10 mb-5"
   >
     <v-card-text>
-      <RaeProductPopUpEmittedForm
-        v-if="initialStatus == 'Emitted'"
-        @cancel="emits('cancel')"
-      />
-      <RaeProductPopUpLdrForm
-        v-else-if="initialStatus == 'LDR'"
-        @cancel="emits('cancel')"
-      />
+      <v-form
+        ref="form"
+        @submit.prevent="submitForm"
+      >
+        <v-select
+          v-model="rae.status"
+          label="Tipo"
+          :items="orderUtils.RAE_STATUS.filter(
+            status => ['Emitted', 'LDR', 'Annulled'].includes(status.value)
+          )"
+          :rules="validation.requiredRules.concat([
+            (value) => {
+              if (value != 'Emitted') return true;
+              return 'Aggiorna stato';
+            }
+          ])"
+        />
+        <v-file-input
+          label="File PDF"
+          accept="application/pdf"
+          :error-messages="fileError"
+          :rules="validation.requiredRules"
+          @change="onFilesSelected"
+        />
+        <div
+          v-if="rae.document?.selectedFile"
+          class="mb-4"
+        >
+          <strong>PDF selezionato</strong>
+
+          <v-card class="pdf-card">
+            <v-card-text class="text-center">
+              <v-icon
+                size="64"
+                color="red"
+              >
+                mdi-file-pdf-box
+              </v-icon>
+
+              <div class="pdf-name mt-2">
+                {{ rae.document.selectedFile.name }}
+              </div>
+            </v-card-text>
+
+            <v-divider />
+
+            <v-card-actions class="justify-center">
+              <v-btn
+                icon="mdi-eye"
+                variant="text"
+                @click="openPdf(rae.document)"
+              />
+
+              <v-btn
+                icon="mdi-delete"
+                variant="text"
+                color="red"
+                @click="rae.document = null"
+              />
+            </v-card-actions>
+          </v-card>
+        </div>
+        <FormButtons
+          :loading="loading"
+          @cancel="emits('cancel')"
+        />
+      </v-form>
     </v-card-text>
   </v-card>
 </template>
 
 <script setup>
-import RaeProductPopUpLdrForm from '@/components/operator/raeProducts/RaeProductPopUpLdrForm';
-import RaeProductPopUpEmittedForm from '@/components/operator/raeProducts/RaeProductPopUpEmittedForm';
+import FormButtons from '@/components/FormButtons';
 
+import { ref } from 'vue';
 import { storeToRefs } from 'pinia';
+import orderUtils from '@/utils/order';
+import { useRouter } from 'vue-router';
+import validation from '@/utils/validation';
 import { useRaeProductStore } from '@/stores/raeProduct';
+
+const form = ref(null);
+const fileError = ref('');
+const loading = ref(false);
+const router = useRouter();
+const emits = defineEmits(['cancel']);
 
 const raeProductStore = useRaeProductStore();
 const { element: rae } = storeToRefs(raeProductStore);
 
-const emits = defineEmits(['cancel']);
-const initialStatus = rae.value.status;
+const submitForm = async () => {
+  if (!(await form.value.validate()).valid) return;
+  loading.value = true;
+
+  raeProductStore.updateElementWithFormData(router, (data) => {
+    loading.value = false;
+    if (data.status == 'ok') {
+      raeProductStore.initList(router);
+      emits('cancel');
+    }
+  });
+};
+
+const onFilesSelected = (event) => {
+  const selectedFile = event.target.files?.[0];
+  if (!selectedFile) return;
+
+  fileError.value = '';
+
+  if (selectedFile.type !== 'application/pdf') {
+    fileError.value = 'Puoi caricare solo file PDF';
+    return;
+  }
+
+  rae.value.document = {
+    selectedFile,
+    preview: URL.createObjectURL(selectedFile),
+  };
+};
 </script>
