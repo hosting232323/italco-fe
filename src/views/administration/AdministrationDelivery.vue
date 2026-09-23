2956516d1ffd4f6ba6bda53261c7b4ebd4495018
cl_cassanelli <cldevofficial@gmail.com>
2026-09-23 21:34:03 +0000

    Il borderò non sceglie più i corrieri, li porta il veicolo

--- src/views/administration/AdministrationDelivery.vue
+++ src/views/administration/AdministrationDelivery.vue
@@ -10,7 +10,7 @@
       />
     </h1><hr>
     <TransportTable />
-    <h1 id="delivery-form">
+    <h1>
       Utenti Delivery
     </h1><hr>
     <DeliveryUserInfoTable />
@@ -21,28 +21,18 @@
   >
     <TransportForm />
   </v-dialog>
-  <v-dialog
-    v-model="deliveryUserInfoForm"
-    max-width="1500"
-  >
-    <DeliveryUserInfoForm />
-  </v-dialog>
 </template>
 
 <script setup>
 import TransportForm from '@/components/administration/transports/TransportForm';
 import TransportTable from '@/components/administration/transports/TransportTable';
-import DeliveryUserInfoForm from '@/components/administration/transports/DeliveryUserInfoForm';
 import DeliveryUserInfoTable from '@/components/administration/transports/DeliveryUserInfoTable';
 
 import { storeToRefs } from 'pinia';
 import { useTransportStore } from '@/stores/transport';
-import { useAdministrationUserStore } from '@/stores/administrationUser';
 
 const transportStore = useTransportStore();
-const administrationUserStore = useAdministrationUserStore();
 const { activeForm: activeTransportForm, element: transport } = storeToRefs(transportStore);
-const { activeForm: deliveryUserInfoForm } = storeToRefs(administrationUserStore);
 
 const openTransportForm = () => {
   transport.value = {};
